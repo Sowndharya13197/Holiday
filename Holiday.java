ff23b627c6421257d919b1b3eca1d15020f7db16
Sowndharya13197 <sowndharyagopal13@gmail.com>
2017-06-18 06:08:21 -0700

    Create Holiday.java

---
+++ Holiday.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.Scanner;
+public class Holiday {
+    public static void main(String args[])
+    {String str;
+    Scanner s=new Scanner(System.in);
+    str=s.nextLine();
+    switch(str)
+            {case "sunday":
+            {System.out.println("Holiday");
+            break;
+            }
+            case "Monday":
+            {System.out.println("Working day");
+        break;
+            }
+             case "tuesday":
+            {System.out.println("Working day");
+        break;
+            }
+             case "wednesday":
+            {System.out.println("Working day");
+        break;
+            }
+             case "thursday":
+            {System.out.println("Working day");
+        break;
+            }
+             case "friday":
+            {System.out.println("Working day");
+        break;
+            }
+             case "saturday":
+            {System.out.println("Holiday");
+        break;
+            }
+        
+    }
+    }
+    
+}
