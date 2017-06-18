5380242f7444dfe82781445572731508ab7bdad8
Sowndharya13197 <sowndharyagopal13@gmail.com>
2017-06-18 06:10:24 -0700

    Update Holiday.java

--- Holiday.java
+++ Holiday.java
@@ -7,31 +7,31 @@ public class Holiday {
     str=s.nextLine();
     switch(str)
             {case "sunday":
-            {System.out.println("Holiday");
+            {System.out.println("false");
             break;
             }
             case "Monday":
-            {System.out.println("Working day");
+            {System.out.println("true");
         break;
             }
              case "tuesday":
-            {System.out.println("Working day");
+            {System.out.println("true");
         break;
             }
              case "wednesday":
-            {System.out.println("Working day");
+            {System.out.println("true");
         break;
             }
              case "thursday":
-            {System.out.println("Working day");
+            {System.out.println("true");
         break;
             }
              case "friday":
-            {System.out.println("Working day");
+            {System.out.println("true");
         break;
             }
              case "saturday":
-            {System.out.println("Holiday");
+            {System.out.println("true");
         break;
             }
         
